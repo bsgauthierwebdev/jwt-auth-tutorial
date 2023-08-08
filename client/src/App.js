454c50707f09b1f323bec3ca0a63114f03bb7f37
Brent Gauthier <cajundrummer@gmail.com>
2023-08-08 10:30:00 -0400

    Built conditional routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,62 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState} from 'react'
+import {
+  BrowserRouter as Router, 
+  Routes, // Switch in tutorial
+  Route, 
+  Navigate, // Redirect in tutorial
+} from 'react-router-dom'
 import './App.css'
 
+// Import components
+import Dashboard from './Components/Dashboard';
+import Login from './Components/Login';
+import Register from './Components/Register';
+
 function App() {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
   return (
     <Fragment>
-      
+      <Router>
+        <div className = 'container'>
+          <Routes>
+            <Route 
+              exact 
+              path = '/login'
+              element = {
+                !isAuthenticated ? (
+                  <Login />
+                ) : (
+                  <Navigate to = {'/dashboard'} />
+                )
+              }
+            />
+            <Route 
+              exact
+              path = '/register'
+              element = {
+                !isAuthenticated ? (
+                  <Register />
+                ) : (
+                  <Navigate to = {'/login'} />
+                )
+              }
+            />
+            <Route 
+              exact
+              path = '/dashboard'
+              element = {
+                isAuthenticated ? (
+                  <Dashboard />
+                ) : (
+                  <Navigate to = '/login' />
+                )
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
     </Fragment>
   );
 }
