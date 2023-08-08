f7dbe9e6bffc5aec296f75a96b403d33b8c06938
Brent Gauthier <cajundrummer@gmail.com>
2023-08-08 11:43:19 -0400

    Built Dashboard component

---
+++ client/src/Components/Dashboard.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react'
+
+const Dashboard = ({setAuth}) => {
+  return (
+    <Fragment>
+        <h1>Dashboard</h1>
+        <button onClick = {() => {setAuth(false)}}>
+          Logout
+        </button>
+    </Fragment>
+  )
+}
+
+export default Dashboard
